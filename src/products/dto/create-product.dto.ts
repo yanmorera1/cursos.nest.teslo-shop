ef92efb493802c41ec6f0e2d2dcd9ade845301dc
YanMorera <90201379+YanMorera@users.noreply.github.com>
2023-11-22 17:41:00 -0500

    class: 128

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,49 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger'
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator'
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  @ApiProperty({ example: 'Camisa', minimum: 1, required: true })
+  title: string
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  @ApiProperty({ required: false })
+  price?: number
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ readOnly: false })
+  description?: string
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({ required: false })
+  slug?: string
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number
+
+  @IsString({ each: true })
+  @IsArray()
+  @ApiProperty({ required: true, isArray: true })
+  sizes: string[]
+
+  @IsString()
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  @ApiProperty({ required: true })
+  gender: string
+}
